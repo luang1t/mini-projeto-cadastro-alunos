a0e813841235f87795b021034e8d1e671a80c617
luang1t <luanzragit@gmail.com>
2025-08-19 22:54:50 -0300

    estrutura menu definida

---
+++ mini-projeto-notas.py
@@ -0,0 +1,26 @@
+import os
+#os.system('cls')
+numero_alunos = 0
+while True:
+    print("="*27)
+    print("1 - CADASTRAR ALUNO E NOTA\n2 - ATUALIZAR DADOS\n3 - MOSTRAR MÉDIA DA TURMA\n4 - MOSTRAR DADOS\n0 - SAIR")
+    print("="*27)
+    seletor = int(input("Digite aqui: "))
+    if seletor == 1:
+        print()
+    elif seletor == 2:
+        print()
+    elif seletor == 3:
+        print()
+    elif seletor == 4:
+        print()
+    elif seletor == 0:
+        break
+    else:
+        print("Código inválido!")
+        print("="*27)
+        print("1 - CADASTRAR ALUNO E NOTA\n2 - ATUALIZAR DADOS\n3 - MOSTRAR MÉDIA DA TURMA\n4 - MOSTRAR DADOS\n0 - SAIR")
+        print("="*27)
+        seletor = int(input("Digite aqui: "))                        
+
+
